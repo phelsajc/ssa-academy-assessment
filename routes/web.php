8549e9774db064bf88cacaf2e806ce3368ebd685
phelsajc <phelsajc@users.noreply.github.com>
2024-08-31 06:47:09 +0800

    update route

--- routes/web.php
+++ routes/web.php
@@ -19,21 +19,19 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware(['auth'])->group(function () {
 
-Route::get('/users', [App\Http\Controllers\UserController::class, 'index'])->name('users.index');
-Route::get('/users/{user}', [App\Http\Controllers\UserController::class, 'show'])->name('users.show');
-Route::get('/user/create', [App\Http\Controllers\UserController::class, 'create'])->name('users.create');
-Route::post('/users', [App\Http\Controllers\UserController::class, 'store'])->name('users.store');
-Route::delete('/users/{user}', [App\Http\Controllers\UserController::class, 'destroy'])->name('users.destroy');
-Route::get('/user/trashed', [App\Http\Controllers\UserController::class, 'trashed'])->name('users.trashed');
-
-Route::patch('/user/{user}/restore', [App\Http\Controllers\UserController::class, 'restore'])->name('users.restore');
-Route::delete('/users/{user}/force-delete', [App\Http\Controllers\UserController::class, 'forceDelete'])->name('users.forceDelete');
-
-Route::get('/users/{user}/edit', [App\Http\Controllers\UserController::class, 'edit'])->name('users.edit');
-Route::put('/users/{user}', [App\Http\Controllers\UserController::class, 'update'])->name('users.update');
-
-
-/* Route::get('/admin/home', [App\Http\Controllers\HomeController::class, 'adminDashboard'])->name
-('admin.index')->middleware('is_admin'); */
+    Route::get('/users', [App\Http\Controllers\UserController::class, 'index'])->name('users.index');
+    Route::get('/users/{user}', [App\Http\Controllers\UserController::class, 'show'])->name('users.show');
+    Route::get('/user/create', [App\Http\Controllers\UserController::class, 'create'])->name('users.create');
+    Route::post('/users', [App\Http\Controllers\UserController::class, 'store'])->name('users.store');
+    Route::delete('/users/{user}', [App\Http\Controllers\UserController::class, 'destroy'])->name('users.destroy');
+    Route::get('/user/trashed', [App\Http\Controllers\UserController::class, 'trashed'])->name('users.trashed');
+    
+    Route::patch('/user/{user}/restore', [App\Http\Controllers\UserController::class, 'restore'])->name('users.restore');
+    Route::delete('/users/{user}/force-delete', [App\Http\Controllers\UserController::class, 'forceDelete'])->name('users.forceDelete');
+    
+    Route::get('/users/{user}/edit', [App\Http\Controllers\UserController::class, 'edit'])->name('users.edit');
+    Route::put('/users/{user}', [App\Http\Controllers\UserController::class, 'update'])->name('users.update');
+    
+});
